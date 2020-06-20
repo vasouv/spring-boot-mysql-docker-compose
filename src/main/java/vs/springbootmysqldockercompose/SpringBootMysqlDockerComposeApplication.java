cb93e501aafd3278610ac1d4bd5d8bd11d7cf86a
Vasilis Souvatzis <vasouv@gmail.com>
2020-06-20 19:44:30 +0300

    project setup

---
+++ src/main/java/vs/springbootmysqldockercompose/SpringBootMysqlDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package vs.springbootmysqldockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlDockerComposeApplication.class, args);
+	}
+
+}
